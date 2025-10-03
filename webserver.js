132578b3265f42051b3ef483582107885841e3ee
seohongik <jericho8901@gmail.com>
2025-10-03 19:13:17 +0900

    hello world json

--- webserver.js
+++ webserver.js
@@ -3,8 +3,9 @@ const hostname = '127.0.0.1';
 const port = 3000;
 const server = createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    res.setHeader('Content-Type', 'application/json'); // 'text/plain'
+    const json = "{key:'key',value:'value'}";
+    res.end(json);
 });
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
