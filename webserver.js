9b5835b4d6bb12d91eb740202c5e1c9436c25fef
seohongik <jericho8901@gmail.com>
2025-10-03 19:35:01 +0900

    디스트럭쳐 문법을 안쓰고 변수에 할당

--- webserver.js
+++ webserver.js
@@ -1,4 +1,4 @@
-const { createServer } = require('node:http');
+/*//const { createServer } = require('node:http'); //디스트럭처 할당
 const hostname = '127.0.0.1';
 const port = 3000;
 const server = createServer((req, res) => {
@@ -8,6 +8,19 @@ const server = createServer((req, res) => {
     res.end(json);
 });
 server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+    // hello node.js
+});*/
+
+const server= require('node:http');
+const hostname = '127.0.0.1';
+const port = 3000;
+server.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json'); // 'text/plain'
+    const json = "{key:'key',value:'value'}";
+    res.end(json);
+}).listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
     // hello node.js
 });
