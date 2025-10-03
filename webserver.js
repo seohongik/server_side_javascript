24b50b3020b23efec2cf0e0e633e29c1e3bd40f7
seohongik <jericho8901@gmail.com>
2025-10-03 20:53:09 +0900

    contoller 단 분리

--- webserver.js
+++ webserver.js
@@ -12,15 +12,26 @@ server.listen(port, hostname, () => {
     // hello node.js
 });*/
 
-const server= require('node:http');
+
+import { createServer } from "node:http";
+import userController from "./controller/userController.js";
+import url from "url";
 const hostname = '127.0.0.1';
 const port = 3000;
-server.createServer((req, res) => {
+const server =createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json'); // 'text/plain'
-    const json = "{key:'key',value:'value'}";
-    res.end(json);
-}).listen(port, hostname, () => {
+    const parsedUrl = url.parse(req.url, true); // url 파싱
+    const path = parsedUrl.pathname;
+    const reqMethod =req.method;
+    if (path === '/user'&&reqMethod.toUpperCase()==="GET") {
+        userController.getUser(req,res);
+        userController.createUser(req,res);
+    }else {
+        res.statusCode=200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end("Hello Node");
+    }
+});
+server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
-    // hello node.js
 });
