e1282ba1344bb474780730df62838b7d48679ac6
seohongik <jericho8901@gmail.com>
2025-10-04 14:57:43 +0900

    콜백 함수 추가

--- underscore.js
+++ underscore.js
@@ -1,6 +1,6 @@
 const _underscore=require('underscore');
 
-const arr=[3,6,9,1,12];
+const arr=[3,6,9,1,10];
 
 console.log(_underscore.first(arr));
 console.log(_underscore.last(arr));
@@ -10,3 +10,8 @@ const obj = [{key:'key1',value:'value1'},{key: 'key2',value: 'value2'}];
 //const objArray=Array.from(obj);
 //console.log(objArray);
 console.log(_underscore.first(obj));
+arr.sort();
+console.log(arr);// [1, 10 , 3, 6, 9];
+
+arr.sort(function (a,b){return b-a});
+console.log(arr);
